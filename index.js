331e489ed48a59b5b7de74790edfb067a2fda773
David Robert <davidrobert@MacBook-Pro-de-David.local>
2022-10-19 11:07:25 +0200

    add crud rest

--- index.js
+++ index.js
@@ -42,6 +42,40 @@ app.get('/artists', (req,res) => {
     res.status(200).json(artists)
 })
 
+app.get('/artists/:id', (req,res) => {
+    const id = parseInt(req.params.id)    
+    const artist = artists.find(artist => artist.id === id)    
+    res.status(200).json(artist)
+})
+
+
+app.post('/artists', (req,res) => {
+    console.log(req.body);
+    artists.push(req.body)    
+    res.status(201).json(artists)
+})
+
+
+app.put('/artists/:id', (req,res) => {    
+    const id = parseInt(req.params.id)    
+    let artist = artists.find(artist => artist.id === id)
+    console.log(id)
+    console.log(req.body);
+    artist.name =req.body.name,
+    artist.description =req.body.description,
+    artist.type =req.body.type,    
+    res.status(200).json(artist)
+})
+
+
+app.delete('/artists/:id', (req,res) => {
+    const id = parseInt(req.params.id)    
+    let artist = artists.find(artist => artist.id === id)    
+    artists.splice(artists.indexOf(artist),1)    
+    res.status(200).json(artists)
+})
+
+
 app.listen(3000, () => {    
     console.log("Serveur à l'écoute")
 })
